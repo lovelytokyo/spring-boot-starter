2ebb3921b925cd67cb0ff61b63598e150b900cd3
oh_minjung <oh_minjung@cyberagent.co.jp>
2016-05-15 14:50:19 +0900

    [2.1.5]CommandLineRunnerの利用

--- hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot-di/src/main/java/com/example/App.java
@@ -1,19 +1,31 @@
 package com.example;
 
-import com.example.app.Frontend;
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
+import com.example.app.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 @EnableAutoConfiguration
 @ComponentScan // このクラスと同じパッケージ以下のクラスを走査。
-public class App {
+public class App implements CommandLineRunner /* CommandLineRunnerインタフェースのrun メソッドがFrontendクラスのrunに相当 */{
+    @Autowired // CommandLineRunner インタフェースをもつクラスは、Dependency Injectionが可能
+    ArgumentResolver argumentResolver;
+    @Autowired
+    Calculator calculator;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.print("Enter 2 numbers like 'a b' : ");
+        Argument argument = argumentResolver.resolve(System.in);
+        int result = calculator.calc(argument.getA(), argument.getB());
+        System.out.println("result =" + result);
+    }
+
     public static void main( String[] args ) {
-        try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-            Frontend frontend = context.getBean(Frontend.class);
-            frontend.run();
-        }
+        SpringApplication.run(App.class, args);
     }
 }
